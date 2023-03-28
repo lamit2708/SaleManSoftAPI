021298b93d3c0ca6c370e5fab32f189e336011d0
Phat <nguyentanphat8694@gmail.com>
2023-03-28 15:36:50 +0700

    update

---
+++ Code/company/PRC/ProductCategory/bus/VSoft.Company.PRC.ProductCategory.Business.UnitTest.Real/Tests/MgmtReal.cs
@@ -0,0 +1,80 @@
+﻿using VegunSoft.Framework.Business.Dto.Request;
+using VSoft.Company.PRC.ProductCategory.Business.Dto.Data;
+using VSoft.Company.PRC.ProductCategory.Business.Dto.Request;
+using VSoft.Company.PRC.ProductCategory.Business.UnitTest.Bases;
+
+namespace VSoft.Company.PRC.ProductCategory.Business.UnitTest.Test.Tests
+{
+    [TestClass]
+    public class MgmtReal : TestProductCategoryMgmt
+    {
+        [TestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public async Task FindAsync(int id)
+        {
+            await TestFindAsync(new MDtoRequestFindByInt()
+            {
+                Id = id,
+            });
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 3)]
+        public async Task FindRangeAsync(int id1, int id2, int id3)
+        {
+            await TestFindRangeAsync(new MDtoRequestFindRangeByInts()
+            {
+                Ids = new[] { id1, id2, id3 },
+            });
+        }
+
+
+        [TestMethod]
+        [DataRow("Phần mềm")]
+        [DataRow("Dự án web")]
+        [DataRow("Dự án mobile")]
+        public async Task TestCreate(string name)
+        {
+            var e = new ProductCategoryDto();
+            e.Name = name;
+           
+            await TestCreateAsync(new ProductCategoryInsertDtoRequest()
+            {
+                Data = e
+            });
+        }
+        [TestMethod]
+        [DataRow(1, "Thiết kế phần mềm 1")] // 
+        [DataRow(2, "Thiết kế website")]
+        [DataRow(3, "Thiết kế ứng dụng mobile")]
+        [DataRow(4, "Xây dựng hệ thống quản lý")]
+        [DataRow(5, "Dịch vụ Marketing")]
+
+        public async Task TestUpdate(int id, string name)
+        {
+            await TestUpdateAsync(new ProductCategoryUpdateDtoRequest()
+            {
+                Data = new ProductCategoryDto()
+                {
+                    Id=id,
+                    Name = name
+                    
+                }
+            });
+        }
+
+        [TestMethod]
+        [DataRow(13)]
+        [DataRow(14)]
+        [DataRow(15)]
+        public async Task DeleteAsync(int id)
+        {
+            await TestDeleteAsync(new ProductCategoryDeleteDtoRequest()
+            {
+                Id = id,
+            });
+        }
+    }
+}
---
+++ Code/company/USR/User/repository/VSoft.Company.USR.User.Repository/Services/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using VegunSoft.Framework.Repository.Id.Efc.Services;
+using VSoft.Company.USR.User.Data.Db.Contexts;
+using VSoft.Company.USR.User.Data.Entity.Models;
+
+namespace VSoft.Company.USR.User.Repository.Services;
+
+public interface IUserRepository : IEfcRepositoryEntityMgmtId<UserDbContext, MUserEntity, int>
+{
+
+    string? GetFullName(int? id);
+
+    Task<string?> GetFullNameAsync(int? id);
+}
