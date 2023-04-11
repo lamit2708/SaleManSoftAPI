0366a206365b8f778e16a997bc487d33b650924e
Nguyen Tan Phat <52371943+nguyentanphat8694@users.noreply.github.com>
2023-04-11 13:12:20 +0700

    u

---
+++ Code/company/DEA/Deal/bus/VSoft.Company.DEA.Deal.Business.Dto/Data/DealTagDto.cs
@@ -0,0 +1,10 @@
+﻿namespace VSoft.Company.DEA.Deal.Business.Dto.Data
+{
+    public class DealTagDto
+    {
+        public long DealId { get; set; } 
+        public string? CustomerName { get; set; }
+        public string? CustomerPhone { get; set; }
+        public double PredictPrice { get; set; }
+    }
+}
